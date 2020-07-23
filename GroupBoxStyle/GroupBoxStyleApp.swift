da94ab3014582087f3a400e7c32482c604a61e59
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-23 14:31:57 +0300

    Initial Commit

---
+++ GroupBoxStyle/GroupBoxStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBoxStyleApp.swift
+//  GroupBoxStyle
+//
+//  Created by ramil on 23.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct GroupBoxStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
